0454f60fb7a49c0d1b76dcb7e6a4ebc7643b1d63
Neko One <death77thebest@gmail.com>
2019-12-08 18:56:00 -0500

    wew

--- index.js
+++ index.js
@@ -47,4 +47,4 @@ const activities_list = [
 
 client.on('error', console.error);
 
-client.login('NjM2Mjk2NzExOTM3MTk2MDU3.XbjnyQ.D-g6CvDyJAgkpiNURGSP8S3fktU');
+client.login('you thought I would really put my token here?');

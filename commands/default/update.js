4e09a3d9680535e97afc917efa180f1ecbeb6ece
Neko One <death77thebest@gmail.com>
2020-01-20 13:31:44 -0500

    Update update.js

--- commands/default/update.js
+++ commands/default/update.js
@@ -11,20 +11,8 @@ module.exports = class AvatarCommand extends Command {
         })
     }
     run(msg, {user}){
-        return message.say('```md
-- 2 New Tanks: IX T92E1, X Sheridan
-- New Ammo Type: 152 mm MGM 51A1E2 Guided Missle with 340 Penetration
-- Grille Concealment Buff 1.45 => 1.86
-- 121B DPM Buff 8.34 => 8.69
-- Foch 155 AP and HE Damage nerf, and Rate of Fire + Magazine Reload Time Buffed
-- Standard B Dispersion Nerfed 0.336 => 0.345
-- Progetto 65 Base Aim Time, Dispersion, Engine Power, Effective Power/Weigh Ratio, and Traverse Speed Nerfed; Per Shell Reload Time Buffed
-- Tier VI-IX Armor Changes
-- New Accumulative Avatars
-- Mobile Now Gets Free XP for Watching Videos and Extra Daily Missions
-- Now Receive In-Game Messages from Friends Only
-- Thumbnail for People Without an Avatar was Changed
-```')
+        return message.say(' 2 New Tanks: IX T92E1, X Sheridan New Ammo Type: 152 mm MGM 51A1E2 Guided Missle with 340 Penetration Grille Concealment Buff 1.45 => 1.86 121B DPM Buff 8.34 => 8.69Foch 155 AP and HE Damage nerf, and Rate of Fire + Magazine Reload Time BuffedStandard B Dispersion Nerfed 0.336 => 0.345Progetto 65 Base Aim Time, Dispersion, Engine Power, Effective Power/Weigh Ratio, and Traverse Speed Nerfed; Per Shell Reload Time Buffed Tier VI-IX Armor Changes New Accumulative Avatars Mobile Now Gets Free XP for Watching Videos and Extra Daily Missions Now Receive In-Game Messages from Friends Only Thumbnail for People Without an Avatar was Changed
+')
     }
         
 }
